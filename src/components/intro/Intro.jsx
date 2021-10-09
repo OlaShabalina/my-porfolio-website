149bee0b0829e6a60c988ea8bd7a9d0e5491986f
OlaShabalina <o.shabalina@outlook.com>
2021-10-09 18:27:29 +1100

    added iTyped package and animated the span text with it. Added additional style for the cursor

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,21 @@
 import './Intro.scss'
 import {KeyboardArrowDownOutlined} from '@material-ui/icons'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
+
+    // code to apply ityped effect on the span
+    const textRef = useRef();
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 40,
+            strings: ["Coding", "JavaScript", "React", "Learning", "HTML", "CSS"]
+        })
+    }, [])
+
     return (
         <div className="Intro" id="intro">
             <div className="left">
@@ -13,7 +27,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi there, I'm</h2>
                     <h1>Olga Shabalina</h1>
-                    <h3>Real Estate Agent who loves <span>Coding</span></h3>
+                    <h3>Real Estate Agent who loves <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <KeyboardArrowDownOutlined className="icon" />
