7e40eec9a0773c917332c5aea630e53d1e3bfc35
OlaShabalina <o.shabalina@outlook.com>
2021-10-09 19:24:58 +1100

    styled the portfolio page (alignment, colors, hover effects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,40 @@ import './Portfolio.scss'
 export default function Portfolio() {
     return (
         <div className="Portfolio" id="portfolio">
-            Test
+            <h1>Porfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>React</li>
+                <li>Express</li>
+                <li>HTML/CSS</li>
+                <li>JavaScript</li>
+            </ul>
+            <div className="container">
+                <div className="project">
+                    <img src="https://user-images.githubusercontent.com/88268603/136360553-f43c77bc-58a0-44e4-bc3b-dec9a39d324c.png" />
+                    <h3>Task manager App</h3>
+                </div>
+                <div className="project">
+                    <img src="https://user-images.githubusercontent.com/88268603/136360553-f43c77bc-58a0-44e4-bc3b-dec9a39d324c.png" />
+                    <h3>Task manager App</h3>
+                </div>
+                <div className="project">
+                    <img src="https://user-images.githubusercontent.com/88268603/136360553-f43c77bc-58a0-44e4-bc3b-dec9a39d324c.png" />
+                    <h3>Task manager App</h3>
+                </div>
+                <div className="project">
+                    <img src="https://user-images.githubusercontent.com/88268603/136360553-f43c77bc-58a0-44e4-bc3b-dec9a39d324c.png" />
+                    <h3>Task manager App</h3>
+                </div>
+                <div className="project">
+                    <img src="https://user-images.githubusercontent.com/88268603/136360553-f43c77bc-58a0-44e4-bc3b-dec9a39d324c.png" />
+                    <h3>Task manager App</h3>
+                </div>
+                <div className="project">
+                    <img src="https://user-images.githubusercontent.com/88268603/136360553-f43c77bc-58a0-44e4-bc3b-dec9a39d324c.png" />
+                    <h3>Task manager App</h3>
+                </div>
+            </div>
         </div>
     )
 }
