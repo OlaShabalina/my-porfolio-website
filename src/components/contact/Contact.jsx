ec75c4afe99cf7ab5a90dd68bbeb7aa64c5d14ae
OlaShabalina <o.shabalina@outlook.com>
2021-10-11 00:08:37 +1100

    created a footer with social links

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,25 +1,43 @@
+import { useState } from 'react';
+import Footer from '../footer/Footer'
 import './Contact.scss'
 
 export default function Contact() {
+
+    // handling submittion of the form
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    }
+
     return (
         <div className="Contact" id="contact">
-            <div className="left">
-                <div className="container">
-                    <img 
-                        src="https://th.bing.com/th/id/OIP.73vyI0NEb-HVf9yjK13ulwHaEB?pid=ImgDet&rs=1"
-                        alt="contact me octopus"
-                    />
+            <div className="content">
+                <div className="left">
+                    <div className="container">
+                        <img 
+                            src="https://th.bing.com/th/id/OIP.73vyI0NEb-HVf9yjK13ulwHaEB?pid=ImgDet&rs=1"
+                            alt="contact me octopus"
+                        />
+                    </div>
+                </div>
+                <div className="right">
+                    <h2>Contact me</h2>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="Your name" />
+                        <input type="text" placeholder="Email" />
+                        <textarea placeholder="Message"></textarea>
+                        <button type="submit">Send</button>
+                        {message && 
+                        <span>
+                            Thank you for your message, I'll be in touch shortly.
+                        </span>}
+                    </form>
                 </div>
             </div>
-            <div className="right">
-                <h2>Contact me</h2>
-                <form>
-                    <input type="text" placeholder="Your name" />
-                    <input type="text" placeholder="Email" />
-                    <textarea placeholder="Message"></textarea>
-                    <button type="submit">Send!</button>
-                </form>
-            </div>
+            <Footer />
         </div>
     )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import './Footer.scss'
+
+export default function Footer() {
+    return (
+        <div className="Footer">
+            <div className="socials">
+                <a href="https://github.com/OlaShabalina" target="_blank">
+                    <i class="devicon-github-original"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/olgashabalina/" target="_blank">
+                    <i class="devicon-linkedin-plain"></i>
+                </a>
+            </div>
+            <p>© 2021 Portfolio of Olga Shabalina</p>
+        </div>
+    )
+}
