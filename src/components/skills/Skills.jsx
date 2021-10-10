4d222af8a919ec5cf28d159fab9bcbc348b94f93
OlaShabalina <o.shabalina@outlook.com>
2021-10-10 22:03:58 +1100

    added styles to the skills page

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -12,12 +12,13 @@ export default function Skills() {
 
     return (
         <div className="Skills" id="skills">
-            <h1>What I have learnt and applied so far:</h1>
+            <h1>What I have <span>learnt</span> and <span>applied</span> so far:</h1>
             <div className="container">
                 {listOfSkills.map(icon => (
                     <i 
                         class={isIconOriginal(icon)}
                         key={icon.id}
+                        title={icon.name}
                     >
                     </i>
                 ))}
