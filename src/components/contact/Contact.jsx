f7e3e01d5ec22f9d75197a8c16dfafeece0bf042
OlaShabalina <o.shabalina@outlook.com>
2021-10-10 22:08:43 +1100

    added a form to the contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,17 @@ import './Contact.scss'
 export default function Contact() {
     return (
         <div className="Contact" id="contact">
-            Test
+            <div className="left">
+                <img src="" />
+            </div>
+            <div className="right">
+                <h2>Contact me</h2>
+                <form>
+                    <input type="text" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send!</button>
+                </form>
+            </div>
         </div>
     )
 }
