1b3698e10ce18c8589882738496b5519d1933b82
OlaShabalina <o.shabalina@outlook.com>
2021-10-10 22:49:19 +1100

    Styled the contact page, useda test img for now, will replace later

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,11 +4,17 @@ export default function Contact() {
     return (
         <div className="Contact" id="contact">
             <div className="left">
-                <img src="" />
+                <div className="container">
+                    <img 
+                        src="https://th.bing.com/th/id/OIP.73vyI0NEb-HVf9yjK13ulwHaEB?pid=ImgDet&rs=1"
+                        alt="contact me octopus"
+                    />
+                </div>
             </div>
             <div className="right">
                 <h2>Contact me</h2>
                 <form>
+                    <input type="text" placeholder="Your name" />
                     <input type="text" placeholder="Email" />
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">Send!</button>
