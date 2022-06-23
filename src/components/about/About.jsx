71dd9cc211ed3cb57a7b281640a0b5f23a4fd97a
Olga <88268603+OlaShabalina@users.noreply.github.com>
2022-06-23 22:59:20 +1000

    change about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,16 +7,16 @@ export default function About() {
                 <small>If you reached here, you may as well get to know me a bit more! ;-) </small>
                 <h1> A few words about myself: </h1>
                 <ul>
-                    <li><strong>Bootcamp & Udemy</strong> graduate who thinks she can now build whatever she wants (with enough research, of course).</li>
-                    <li>I volunteer for <strong>Inco web-app project</strong>, learning to work in a big team and my contribution is => <a 
-                        href="https://github.com/OlaShabalina/incode-web-app" target="_blank"
+                    <li>Currently working as a <strong>Data Engineer</strong> for <strong>Rea Group</strong>. Lots of SQL, Python, AWS and BigQuery.</li>
+                    <li>Changed career multiple times before finding a passion for coding. Ex researcher (economics), marketing specialist and real estate agent (sales).</li>
+                    <li>I volunteer for <strong>Code for Australia</strong>, if you would like to join me (code, design, project management, creativity, etc) - don't hesitate to reach out => <a 
+                        href="https://cfa-website.vercel.app/get-involved/civic-makers#:~:text=What%20is%20Civic%20Makers%3F,the%20lens%20of%20civic%20tech." target="_blank"
                         rel="noreferrer"
-                        title="Github Fork of the Inco project"
+                        title="Link to Code for Australia"
                     >
-                        here
+                        more info on what we do is here
                     </a>.</li>
-                    <li>Currently working as a <strong>real estate agent</strong> => helping people to sell/buy their home (or a few investment properties).</li>
-                    <li>I have a <strong>master degree in economics</strong> and a few other degrees/courses (more info => <a 
+                    <li>I have uni degrees including <strong>master in economics</strong> although I'm now convinced it wasn't necessary at all to succeed in life (my Linkedin => <a 
                         href="https://www.linkedin.com/in/olgashabalina/" target="_blank"
                         rel="noreferrer"
                         title="LinkedIn"
@@ -24,8 +24,8 @@ export default function About() {
                         here
                     </a>).  
                     </li>
-                    <li>Lived and worked/studied in 4 countries, speak Russian, Italian and a bit of Hindi & Chinese. </li>
-                    <li>Love animals, coffee and <strong>connecting with people</strong>.
+                    <li>Lived and worked in 4 countries, speak Russian, Italian and a bit of Hindi & Chinese. It made a lot of sense to learn some programming ones as well :-) </li>
+                    <li>Love animals, coffee, Jack London (when Netflix is off) and <strong>connecting with people</strong>.
                     </li>
                 </ul>
 
